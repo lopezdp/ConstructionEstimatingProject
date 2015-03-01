0966e5a6a75c0a312d86b6c66a3b490bce692479
David Lopez <lopezdp@users.noreply.github.com>
2015-03-01 08:09:08 -0500

    Cosmetic Update to feetInchesCalc.py
    updated the format of the out put statements printed by feeInchesCalc.py

--- feetInchesCalc.py
+++ feetInchesCalc.py
@@ -8,6 +8,7 @@ def measurements():
     program_continue = input("Do you want to keep a running total of your measurements? 1 for yes, 2 for no: ");
 
     if (program_continue == 1):
+        print " "
         feet = input("Enter feet portion of distance: ");
         inch = input("Enter inches portion of distance: ");
         subTotal += (float(feet) + (float(inch)/12))
@@ -17,18 +18,21 @@ def measurements():
         programOn = False
         return total
     else:
+        print " "
         program_continue = input("ERROR!!! Do you want to keep adding to your running total? 1 for yes, 2 for no: ");
     
     while(programOn):
-
+        print " "
         program_continue = input("Do you want to keep adding to your running total? 1 for yes, 2 for no: ");
         
         if (program_continue == 1):
+            print " "
             feet = input("Enter feet portion of distance: ");
             inch = input("Enter inches portion of distance: ");
             subTotal += (float(feet) + (float(inch)/12))
         elif (program_continue == 2):
-            print "Your measurement in Feet is: ", subTotal
+            print " "
+            print "Your measurement in FEET is: ", subTotal
             total = subTotal
             meters = total * 0.3048
             print "Your measurement in METERS is: ", meters
